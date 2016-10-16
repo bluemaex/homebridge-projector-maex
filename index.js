f1843a3be497c66c44a8ad44cd2ab69236faf6d4
Max Stockner <mail@bluemaex.de>
2016-10-16 16:50:02 +0200

    ignore command if the projector is already in requested state

--- index.js
+++ index.js
@@ -24,35 +24,47 @@ ProjectorAccessory.prototype.getFritzEnergy = function() {
     .getSessionID(config.username, config.password, config.options)
       .then((sid) => {
         return fritz.getSwitchPower(sid, switchId, config.options).then((energy) => {
-          this.log("Energy state [" + switchId + "]: " + energy, "treshold " + treshold)
+          this.log(`Energy usage [${switchId}]: ${energy}, treshold is ${treshold}`)
           return energy > treshold ? 1 : 0
         })
       })
 }
 
+ProjectorAccessory.prototype.sendSshCommand = function(command) {  
+  return new Promise((resolve, reject) => {
+    const stream = ssh(command, this.config.ssh)
+    
+    stream.on('error',  err => {
+      this.log(`Error occured: ${error}`)
+      reject(err || new Error(`Error executing ${command}`))
+    })
+
+    stream.on('finish', () => {
+      this.log(`Executed command "${command}" successfully`)
+      resolve()
+    })
+  })
+}
+
 ProjectorAccessory.prototype.getState = function(callback) {
-  this.getFritzEnergy().then((state) => {
-    this.log('got', state)
-    callback(null, state)
-  })
-  .catch((error) => {
-    this.log(error)
-    callback(err || new Error('Error getting state of ' + this.name))
-  })
+  this.getFritzEnergy().then(state => { callback(null, 1) })
+      .catch(error => { callback(error || new Error(`Error getting state of ${this.name}`)) })
 }
 
 ProjectorAccessory.prototype.setState = function(power, callback) {
   const command = (power == 1) ? this.config.on : this.config.off
-  const stream = ssh(command, this.config.ssh)
 
-  stream.on('error',  (err) => {
-    this.log('Error: ' + err)
-    callback(err || new Error('Error setting ' + this.name + ' to ' + power))
-  })
-
-  stream.on('finish', () => {
-    this.log('Set ' + this.name + ' to ' + power)
-    callback(null)
+  this.getFritzEnergy().then(state => {
+    if(power !== state ) {
+      this.log(`state of projector is ' + state + ', requested to switch to ' + power`)
+      this.sendSshCommand(command).then(() => { callback() })
+    } else {
+      this.log(`already in state ${state}, ignoring`)
+      callback()
+    }
+  }).catch(error => {
+    this.log(error)
+    callback(error || new Error(`Error setting ${state} of ${this.name} to value`))
   })
 }
 
